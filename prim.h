66fa58e8fad44cdb15a5ec6d85a5403f5900bad7
Max Bruegger <brueggerm2533@my.uwstout.edu>
2026-04-01 12:50:04 -0500

    should be working

--- prim.h
+++ prim.h
@@ -1,1 +1,18 @@
 //Please put header information in this file.
+#pragma once
+
+#include <vector>
+#include <climits>
+
+// Type aliases shared across files
+using Edge = std::pair<int, int>;
+using Graph = std::vector<std::vector<Edge>>;
+
+/**
+ * Prim's Algorithm - Minimum Spanning Tree
+ *
+ * @param graph     Adjacency list of the graph
+ * @param start     Starting vertex
+ * @param numVerts  Total number of vertices
+ */
+void primsAlgorithm(const Graph& graph, int start, int numVerts);
